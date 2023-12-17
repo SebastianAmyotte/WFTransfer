945bc8b1ae3d6f154ef599dba157e5d6bc119937
Sebastian Amyotte <amyots21@uwosh.edu>
2023-12-17 15:49:27 -0600

    ADB replaces MTP

--- MTPAPI/SRC/Functions.cs
+++
@@ -1,7 +0,0 @@
-static class Functions {
-    static public String LIST = "LIST";   // Lists devices to file
-    static public String READ = "READ";   // Reads a folder from device
-    static public String WRITE = "WRITE"; // Writes a folder to device
-
-    static public string XFER = "XFER";  // Copy a folder on device
-}
